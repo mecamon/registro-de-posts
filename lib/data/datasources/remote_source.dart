9d0d588074e5178344d6a3ce8f00e8c449caa920
Meca <mejiacarlosdev@gmail.com>
2021-03-23 21:06:46 -0400

    Changes to the repository

--- lib/data/datasources/remote_source.dart
+++ lib/data/datasources/remote_source.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'package:registro_de_posts/data/models/posts_model.dart';
-import 'package:registro_de_posts/data/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 const headers = {'Content-Type': 'application/json'};
@@ -8,11 +5,11 @@ const headers = {'Content-Type': 'application/json'};
 abstract class RemoteSource {
   //This method will return a list of UserModel objects formatted
   //from the Json response in the site https://jsonplaceholder.typicode.com/photos
-  Future<List<UserModel>> getAllUsers();
+  Future getAllUsers();
 
   //This method will return a list of PostsModel objects formatted
   //from the Json response in the site https://jsonplaceholder.typicode.com/photos
-  Future<List<PostsModel>> getAllPosts();
+  Future getAllPosts();
 }
 
 
@@ -22,47 +19,28 @@ class RemoteSourceImpl implements RemoteSource {
   RemoteSourceImpl(this.client);
 
   @override
-  Future<List<PostsModel>> getAllPosts() => 
+  Future getAllPosts() => 
     _getAllPosts('https://jsonplaceholder.typicode.com/posts');
 
   @override
-  Future<List<UserModel>> getAllUsers() => 
+  Future getAllUsers() => 
     _getAllUsers('https://jsonplaceholder.typicode.com/users');
 
 
 
-  Future<List<PostsModel>> _getAllPosts(String url) async {
-
-    final response = await client.get(Uri.parse(url), headers: headers);
-    final List<PostsModel> resultingList = [];
-
-    if (response.statusCode == 200) {
-      List data = json.decode(response.body);
-
-      for(var jsonItem in data) {
-        final post = PostsModel.fromJson(jsonItem);
-        resultingList.add(post);
-      }
-    }
-    return resultingList;
+  Future _getAllPosts(String url) async {
+    final http.Response response = 
+      await client.get(Uri.parse(url), headers: headers);
+    
+    return response;
   }
 
 
-  Future<List<UserModel>> _getAllUsers(String url) async {
+  Future _getAllUsers(String url) async {
+    final http.Response response = 
+      await client.get(Uri.parse(url), headers: headers);
 
-    final response = await client.get(Uri.parse(url), headers: headers);
-    final List<UserModel> resultingList = [];
-
-    if (response.statusCode == 200) {
-      List data = json.decode(response.body);
-
-      for(var jsonItem in data) {
-        final user = UserModel.fromJson(jsonItem);
-
-        resultingList.add(user);
-      }
-    }
-    return resultingList;
+    return response;
   }
 
 }
--- lib/data/repositories/remote_source_repository.dart
+++ lib/data/repositories/remote_source_repository.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:registro_de_posts/data/datasources/remote_source.dart';
 import 'package:registro_de_posts/data/models/posts_model.dart';
 import 'package:registro_de_posts/data/models/user_model.dart';
+import 'package:http/http.dart' as http;
 
 class RemoteSourceRepository {
   final RemoteSourceImpl remoteDataSource;
@@ -8,11 +11,36 @@ class RemoteSourceRepository {
   RemoteSourceRepository({this.remoteDataSource});
 
   Future<List<PostsModel>> getAllposts() async {
-    return await remoteDataSource.getAllPosts();
+
+    http.Response response = await remoteDataSource.getAllPosts();
+    final List<PostsModel> resultingList = [];
+
+    if (response.statusCode == 200) {
+      List data = json.decode(response.body);
+
+      for(var jsonItem in data) {
+        final post = PostsModel.fromJson(jsonItem);
+        resultingList.add(post);
+      }
+    }
+    return resultingList;
   }
 
   Future<List<UserModel>> getAllUsers() async {
-    return await remoteDataSource.getAllUsers();
+
+    http.Response response = await remoteDataSource.getAllUsers();
+    final List<UserModel> resultingList = [];
+
+    if (response.statusCode == 200) {
+      List data = json.decode(response.body);
+
+      for(var jsonItem in data) {
+        final user = UserModel.fromJson(jsonItem);
+
+        resultingList.add(user);
+      }
+    }
+    return resultingList;
   }
 
 }
